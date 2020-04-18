95bd34179d447fabead8ffbb22630c80431ed2ad
Fernando Santoro <fsantoro@psfc.mit.edu>
2020-04-17 20:39:19 -0400

    Rewrite of the main loop to fix a bad behaviour that was giving the right answer anyway...

--- acq400_hapi/user_apps/user_ttimes.py
+++ acq400_hapi/user_apps/user_ttimes.py
@@ -21,6 +21,7 @@ import numpy as np
 from MDSplus import Tree
 import sys
 import csv
+import copy
 
 def setTransitionTimes(treeName, nchan, delta, stlpath):
     #This function is kind of a Unit Test.
@@ -94,15 +95,16 @@ def set_stl(treeName, times, nchan, stlpath):
 
     # initialize the state matrix
     rows, cols = (len(t_times), nchan)
-    state = [[0]*cols]*rows
+    state = [[0]*cols]
+    
+    print(state)
 
     # Building the state matrix. For each transition times given by t_times, we look for those times that
     # appear in the channel. If a transition time does not appear in that channel, then the state
     # doesn't change.
 
-    i=0
-    for t in t_times:
-        # print(i, state[i])     
+    for i in range(len(t_times)):
+        print(i, state[i])     
         for j in range(nchan):
             chan_t_states = tree.getNode('ACQ2106_WRPG:OUTPUT_%3.3d' % (j+1))
             
@@ -112,11 +114,13 @@ def set_stl(treeName, times, nchan, stlpath):
             for s in range(len(chan_t_states[0])):
                 
                 #Check if the transition time is one of the times that belongs to this channel:
-                if t in chan_t_states[0][s]:
-                    # print("inside Chan%i" %(j+1), t, i, j, int(np.asscalar(chan_t_states[1][s])), s)
-                    state[i][j] = int(np.asscalar(chan_t_states[1][s]))
+                if t_times[i] == chan_t_states[0][s][0]:
+                    print("inside ", int(chan_t_states[1][s][0]))
+                    state[i][j] = int(chan_t_states[1][s][0])
 
-            print(t, i, j, "state: ", state[i][j])
+            print(t_times[i], "channel: ", j + 1, "state: ", state[i][j])
+            print(i, state[i])
+            print(state)    
 
         # Building the string of 1s and 0s for each transition time:
         binstr = ''
@@ -124,28 +128,25 @@ def set_stl(treeName, times, nchan, stlpath):
             binstr += str(element)
         states_bits.append(binstr)
         
-        i+=1
+        state.append(copy.deepcopy(state[i]))
+        # state.append(state[i])
 
     print(states_bits)
-
-    #Converting those strings into HEX numbers
-    for elements in states_bits:
-        states_hex.append(hex(int(elements,2))[2:]) # the [2:] is because I don't need to 0x part of the hex string
+    print(state)
 
     # Converting the original units of the transtion times in seconds, to micro-seconts:
     times_usecs = []
     for elements in t_times:
         times_usecs.append(int(elements * 1E6)) #in micro-seconds
     # Building a pair between the t_times and hex states:
-    state_list = zip(times_usecs, states_hex)
+    state_list = zip(times_usecs, states_bits)
 
-    outputFile=open(stlpath, 'w')
+    f=open(stlpath, 'w')
 
-    with outputFile as f:
-        writer = csv.writer(f, delimiter=',')
-        writer.writerows(state_list)
+    for s in state_list:
+        f.write('%d,%08X\n' % (s[0], int(s[1], 2)))
 
-    outputFile.close()
+    f.close()
 
 
 if __name__ == '__main__':
