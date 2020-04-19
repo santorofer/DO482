0803ed4a3360b90869e5cb1e8e18e802a97c5266
Fernando Santoro <fsantoro@psfc.mit.edu>
2020-04-19 00:20:58 -0400

    New logic for the creation of the state matrix

--- acq2106_WRPG.py
+++ acq2106_WRPG.py
@@ -105,82 +105,79 @@ class ACQ2106_WRPG(MDSplus.Device):
         with open(example_stl, 'r') as fp:
             uut.load_wrpg(fp.read(), uut.s0.trace)
 
-    def set_stl(self):
-        nchan = 32
+def set_stl(self):
+    nchan = 32
 
-        states_hex    = []
-        states_bits   = []
-        all_t_times   = []
-        all_t_times_states = []
+    states_hex    = []
+    states_bits   = []
+    all_t_times   = []
+    all_t_times_states = []
 
-        # Selecting unique transition times from all the channels:
-        for i in range(nchan):
-            chan_t_times = self.__getattr__('OUTPUT_%3.3d' % (i+1))
+    for i in range(nchan):
+        chan_t_times = self.__getattr__('OUTPUT_%3.3d' % (i+1))
 
-            # Pair of (transition time, state) for each channel:
-            chan_t_states = chan_t_times.data()
+        # Pair of (transition time, state) for each channel:
+        chan_t_states = chan_t_times.data()
 
-            # Creation of an array that contains as EVERY OTHER element all the transition times in it, adding them
-            # for each channel:
-            for x in np.nditer(chan_t_states):
-                all_t_times_states.append(x) #Appends arrays made of one element,
+        # Creation of an array that contains as EVERY OTHER element all the transition times in it, adding them
+        # for each channel:
+        for x in np.nditer(chan_t_states):
+            all_t_times_states.append(x) #Appends arrays made of one element,
 
-        # Choosing only the transition times from the appened array:
-        all_t_times = all_t_times_states[0::2]
+    # Choosing only the transition times:
+    all_t_times = all_t_times_states[0::2]
 
-        # Removing duplicates and then sorting in ascending manner:
-        t_times = []
-        for i in all_t_times:
-            if i not in t_times:
-                t_times.append(i)
+    # Removing duplicates and then sorting in ascending manner:
+    t_times = []
+    for i in all_t_times:
+       if i not in t_times:
+          t_times.append(i)
 
-        # t_times contains the unique set of transitions times used in the experiment:
-        t_times = sorted(np.float64(t_times))
+    # t_times contains the unique set of transitions times used in the experiment:
+    t_times = sorted(np.float64(t_times))
+    print(t_times)
 
-        # initialize the state matrix
-        rows, cols = (len(t_times), nchan)
-        state = [[0]*cols]*rows
+    # initialize the state matrix
+    cols = nchan
+    state = [[0]*cols]
+    
+    # Building the state matrix. For each transition times given by t_times, we look for those times that
+    # appear in the channel. If a transition time does not appear in that channel, then the state
+    # doesn't change.
 
-        # Building the state matrix. For each transition times given by t_times, we look for those that
-        # appear in the channel. If a transition time does not appear in that channel, then the state
-        # for this transition time is consider the same as the previous state for this channel (i.e. the state
-        # hasn't changed)
-        i=0
-        for t in t_times:
-            for j in range(nchan):
-                chan_t_states = self.__getattr__('OUTPUT_%3.3d' % (j+1))
-                
-                for s in range(len(chan_t_states[0])):
-                    #Check if the transition time is one of the times that belongs to this channel:
-                    if t in chan_t_states[0][s]:
-                        state[i][j] = int(np.asscalar(chan_t_states[1][s]))
-
-            # Building the string of 1s and 0s for each transition time:
-            binstr = ''
-            for element in np.flip(state[i]):
-                binstr += str(element)
-            states_bits.append(binstr)
+    for i in range(len(t_times)):
+        for j in range(nchan):
+            chan_t_states = self.__getattr__('OUTPUT_%3.3d' % (j+1))
             
-            i+=1
+            # chan_t_states its elements are pairs of [ttimes, state]. e.g [[0.0, 0],[1.0, 1],...]
+            # chan_t_states[0] are all the first elements of those pairs, i.e the trans. times: e.g [[1D0], [2D0], [3D0], [4D0] ... ]
+            # chan_t_states[1] are all the second elements of those pairs, the states: .e.g [[0],[1],...]
+            for s in range(len(chan_t_states[0])):
+                
+                #Check if the transition time is one of the times that belongs to this channel:
+                if t_times[i] == chan_t_states[0][s][0]:
+                    print("inside ", int(chan_t_states[1][s][0]))
+                    state[i][j] = int(chan_t_states[1][s][0]) 
 
+        # Building the string of 1s and 0s for each transition time:
+        binstr = ''
+        for element in np.flip(state[i]):
+            binstr += str(element)
+        states_bits.append(binstr)
+        
+        state.append(copy.deepcopy(state[i]))
 
-        #Converting those strings into HEX numbers
-        for elements in states_bits:
-            states_hex.append(hex(int(elements,2))[2:]) # the [2:] is because I don't need to 0x part of the hex string
+    # Converting the original units of the transtion times in seconds, to micro-seconts:
+    times_usecs = []
+    for elements in t_times:
+        times_usecs.append(int(elements * 1E6)) #in micro-seconds
+    # Building a pair between the t_times and hex states:
+    state_list = zip(times_usecs, states_bits)
 
-        # Converting the original units of the transtion times in seconds, to micro-seconts:
-        times_usecs = []
-        for elements in t_times:
-            times_usecs.append(int(elements * 1E6)) #in micro-seconds
-        # Building a pair between the t_times and hex states:
-        state_list = zip(times_usecs, states_hex)
+    f=open(self.stl_file.data(), 'w')
 
-        #For example, stlpath = '/home/fsantoro/HtsDevice/acq400_hapi/user_apps/STL/do_states.stl'
-        outputFile=open(self.stl_file.data(), 'w')
+    for s in state_list:
+        f.write('%d,%08X\n' % (s[0], int(s[1], 2)))
 
-        with outputFile as f:
-            writer = csv.writer(f, delimiter=',')
-            writer.writerows(state_list)
-
-        outputFile.close()
+    f.close()
 
